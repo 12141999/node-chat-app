5008d90210ce7447c0f956a7a9bf23097b20b8f9
Robin Jain <robinjain9587@gmail.com>
2019-03-06 22:49:38 +0530

    vngkdfs

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const socketIO = require('socket.io');
+const http = require('http');
 
-const publicPath = path.join(__dirname + '../public');
+const publicPath = path.join(__dirname + '../views');
 let app = express();
 app.use(express.static(publicPath));
 //app.set("view engine" , "ejs");
+
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection' , (socket) => {
+    console.log('new user connected' , socket.id);
+
+    socket.on('disconnect' , () => {
+        console.log('user is disconnect');
+    });
+
+});
+
 
 const port = process.env.PORT || 3000 ;
 
@@ -13,6 +28,6 @@ app.get("/" , (req,res) => {
     res.render("index.ejs");
 });
 
-app.listen(port , () => {
+server.listen(port , () => {
     console.log('server is started');
 });
