cddbde1fa671aa297ddaa504db79a18f2e7ee2a6
Robin Jain <robinjain9587@gmail.com>
2019-03-06 22:30:18 +0530

    thire

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const publicPath = path.join(__dirname + '../public');
 let app = express();
 app.use(express.static(publicPath));
+//app.set("view engine" , "ejs");
 
 const port = process.env.PORT || 3000 ;
+
+app.get("/" , (req,res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port , () => {
     console.log('server is started');
