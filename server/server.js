aca22c21bb685b76a79c47d5f44b36fe9d5d169b
Robin Jain <robinjain9587@gmail.com>
2019-03-06 18:35:41 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname + '/../public');
+let app = express();
+app.use(express.static(publicPath));
+
+const port = process.env.port || 3000 ;
+
+app.listen(port , () => {
+    console.log('server is started');
+});
