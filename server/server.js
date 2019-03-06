a6748951530c6c9271eeeebf40cadc567caaacac
Robin Jain <robinjain9587@gmail.com>
2019-03-06 22:15:56 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 
-const publicPath = path.join(__dirname + '/../public');
+const publicPath = path.join(__dirname + '../public');
 let app = express();
 app.use(express.static(publicPath));
 
-const port = process.env.port || 3000 ;
+const port = process.env.PORT || 3000 ;
 
 app.listen(port , () => {
     console.log('server is started');
